34d6e5dfc252c4ee587fa92155aa6fcdfb6da21b
MaazDanish <dk599318@gmail.com>
2023-08-23 10:40:04 +0530

    Added FileSytem

--- my-app.js
+++ my-app.js
@@ -1,1 +1,6 @@
+//  fs stand for File System - we are importing it here 
+const fileSystem = require('fs'); 
 console.log('hello');
+fileSystem.writeFileSync('fileName.txt','The Data we will put in to filename.txt from node js will come here as arg');
+
+
