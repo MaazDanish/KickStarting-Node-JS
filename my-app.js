d2f112e193988da02f464be7fd6a5b947d2da7ae
MaazDanish <dk599318@gmail.com>
2023-08-23 13:12:46 +0530

    Task 4th Done

--- my-app.js
+++ my-app.js
@@ -1,24 +1,14 @@
-const arrayOne = ['apple', 'oranges', ' ', 'mango', ' ', 'lemon'];
-const arrayTwo = arrayOne.map((val) => {
-    ele = 'empty String'
-    if (val === ' ') {
-        val = ele;
-    }
-    // console.log(ele +" " + val);
-    return val;
-});
-console.log(arrayTwo);
-// --------------------------------
-const obj1 = { 'key1': 1, 'key2': 2 }
-const obj2 = { ...obj1, key1: 1000 }
-console.log(obj1)
-console.log(obj2)
-// -------------------
-const obj3 = { 'key1': 1 }
-const obj4 = { ...obj1 }
-if (obj3 === obj4) {
-    console.log('same objects')
-}
-else {
-    console.log('different objects')
-}
+// const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
+// const { key1, key3} = { ...obj1}
+// console.log(key1, key3)
+
+// const arr1 = ['value1', 'value2']
+// const [ val1, val2 ] = arr1
+// console.log(val1)
+// console.log(val2)
+
+const obj2 = {'key4': 1, "key5": 2, "key6": 1000}
+let { key4, key6} = obj2
+key4 = 20;
+key6 = 123
+console.log(obj2.key4, obj2.key6)
